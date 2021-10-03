c8300ee00cd7c777ce26ae41f4ee4510a0f4b250
eladnisser <enisser@gmail.com>
2021-10-03 12:52:15 +0300

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Elad!'));
+app.get('/', (req, res) => res.send('This one has changed!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
